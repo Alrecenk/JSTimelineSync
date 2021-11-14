d07f1a4680e69bd6f30dfec48261189516e422b1
Alrecenk <alrecenk@gmail.com>
2021-11-14 13:01:34 -0500

    rearrange folders

--- timeline_core/TEvent.js
+++ timeline_core/TEvent.js
@@ -2,7 +2,7 @@
 //To use automatic timeline sychronization you must implement everything that changes the state as an
 // event that occurs at a specific time. This is the base class or doing so.
 
-class TEvent{
+class TimeEvent{
 
 
 
