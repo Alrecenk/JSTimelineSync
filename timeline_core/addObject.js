facc638ff4595bf80c2930182825c2b0be352d89
Alrecenk <alrecenk@gmail.com>
2021-11-16 09:53:34 -0500

    typos

--- timeline_core/addObject.js
+++ timeline_core/addObject.js
@@ -1,5 +1,5 @@
-//All eits ot objects have to take place as events so adding objects to the timeline must alos be an event.
-// You can all addObject on the timelines and itwill create this for you
+//All edits to objects have to take place as events so adding objects to the timeline must also be an event.
+// You can use addObject on the timelines and it will create this for you.
 class addObject extends TEvent{
 
     constructor(obj, time){
