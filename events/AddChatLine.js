313bf3de12721cae3ee50071f904e30b113a94dc
Alrecenk <alrecenk@gmail.com>
2021-11-23 15:14:53 -0500

    remove duplicate event constructors

--- events/AddChatLine.js
+++ events/AddChatLine.js
@@ -1,10 +1,4 @@
 class AddChatLine extends TEvent{
-    
-    /*constructor(time, params){
-        super(time);
-        this.parameters = params;
-    }*/
-
     run(timeline){
         let chat_log = timeline.get(this.parameters.chat_id);
         chat_log.chat.push(this.parameters.line);
--- events/MovePlayer.js
+++ events/MovePlayer.js
@@ -1,10 +1,4 @@
 class MovePlayer extends TEvent{
-
-   /* constructor(time, params){
-        super(time);
-        this.parameters = params;
-    }*/
-
     run(timeline){
         let player = timeline.get(this.parameters.player_id);
         player.x += player.vx * this.parameters.interval;
